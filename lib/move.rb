a3a7613a53b27fbdc8004a7dd7d0d0d46d1aee9b
Inga <ijaanimets@gmail.com>
2020-09-08 23:40:42 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -9,15 +9,13 @@ end
 # code your input_to_index and move method here!
 #def input_to_index(user_input)
   
-  #puts "1: #{board[0]}, 2: #{color[1]}, 3: #{color[2]}, 4: # #{color[3]}, 5: #{color[4]}, 6: #{color[5]}, 7: #{color[6]}, ## : #{board[7]}, 9: #{board[8]}"
 
 def input_to_index(user_input)
   user_input = user_input.to_i
   user_input = user_input - 1
 end
 
-#def move(char)
-#end
+
 def move(board, index, character = "X")
 board[index] = character
 end
